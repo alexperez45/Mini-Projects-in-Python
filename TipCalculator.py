df128d03d960492e06d5a48bf56fc474c888e667
alexperez45 <alperez296@gmail.com>
2024-04-22 11:58:14 -0400

    Removed variable and made code more efficient

--- TipCalculator.py
+++ TipCalculator.py
@@ -5,6 +5,5 @@ people = float (input ("How many people will split the bill? "))
 tip_amount = bill * (tip / 100)
 total_bill = bill + tip_amount
 bill_per_person = total_bill / people
-rounded_amount = "{:.2f}".format(bill_per_person)
-print (f"Each person should pay ${rounded_amount}")
-#test comment
+print ("The total bill is $" + "{:.2f}".format(total_bill))
+print ("Each person should pay $" + "{:.2f}".format(bill_per_person))
