dead753513963dd7c211c19bd3375b62b5d00c3b
alexperez45 <alperez296@gmail.com>
2024-04-21 22:24:03 -0400

    Shortened code

--- Leap_year.py
+++ Leap_year.py
@@ -1,14 +1,13 @@
 # User enters year
 year = int(input("Type a year: "))
-year_by_4 = year % 4 
-year_by_100 = year % 100
-year_by_400 = year % 400
-
-if year_by_4 > 0:
+# year_by_4 = year % 4 
+# year_by_100 = year % 100
+# year_by_400 = year % 400
+if year % 4 > 0:
   print ("Not leap year")
-elif year_by_100 > 0:
+elif year % 100 > 0:
   print ("Leap year")
-elif year_by_400 == 0:
+elif year % 400 == 0:
     print ("Leap year")
 else:
   print ("Not leap year")
