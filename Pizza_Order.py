044f58edb3c76e0be0ac31b67a9ae736d156c6de
alexperez45 <alperez296@gmail.com>
2024-04-22 08:35:16 -0400

    Pizza ordering program

---
+++ Pizza_Order.py
@@ -0,0 +1,22 @@
+print("Welcome to your local Python Pizzeria!")
+size = input("What size pizza do you want? S, M, or L: ")
+add_pepperoni = input("Do you want pepperoni? Y or N: ")
+extra_cheese = input("Do you want extra cheese? Y or N: ")
+bill = 0
+
+if size.upper() == 'S':
+    bill += 15
+elif size.upper() == 'M':
+    bill += 20
+elif size.upper() == 'L':
+    bill += 25
+
+if add_pepperoni.upper() == 'Y' and size == 'S':
+    bill += 2
+elif add_pepperoni.upper() == 'Y':
+    bill += 3
+
+if extra_cheese.upper() == 'Y':
+    bill += 1
+
+print (f'Your final bill is: ${bill}.')
