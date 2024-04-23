683221786443dc44c387f741c4af0c9186056f1d
alexperez45 <alperez296@gmail.com>
2024-04-22 20:16:45 -0400

    Silly love calculator

---
+++ Love_Calculator.py
@@ -0,0 +1,23 @@
+print("The Love Calculator is calculating your score...")
+name1 = input("What is your name? ")
+name2 = input("What is their name? ")
+
+name1 = name1.upper()
+name2 = name2.upper()
+
+nameTrue1 = name1.count("T") + name2.count("T")
+nameTrue2 = name1.count("R") + name2.count("R")
+nameTrue3 = name1.count("U") + name2.count("U")
+nameTrue4 = name1.count("E") + name2.count("E")
+
+nameLove1 = name1.count("L") + name2.count("L")
+nameLove2 = name1.count("O") + name2.count("O")
+nameLove3 = name1.count("V") + name2.count("V")
+nameLove4 = name1.count("E") + name2.count("E")
+
+true_total = nameTrue1 + nameTrue2 + nameTrue3 + nameTrue4
+love_total = nameLove1 + nameLove2 + nameLove3 + nameLove4
+total = int(str(true_total) + str(love_total))
+
+print (f"Your score is {total}.")
+
