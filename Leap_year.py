05dba8707cc61d732d4a79162bf61b3516b2b9fc
alexperez45 <alperez296@gmail.com>
2024-05-18 10:24:27 -0400

    Added f-strings

--- Leap_year.py
+++ Leap_year.py
@@ -1,13 +1,11 @@
 # User enters year
 year = int(input("Type a year: "))
-# year_by_4 = year % 4 
-# year_by_100 = year % 100
-# year_by_400 = year % 400
+
 if year % 4 > 0:
-  print ("Not leap year")
+  print (f"{year} is NOT a leap year")
 elif year % 100 > 0:
-  print ("Leap year")
+  print (f"{year} is a leap year!")
 elif year % 400 == 0:
-    print ("Leap year")
+    print (f"{year} is a leap year!")
 else:
-  print ("Not leap year")
+  print (f"{year} is NOT a leap year")
