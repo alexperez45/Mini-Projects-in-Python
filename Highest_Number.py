9c19061b53671984c5f0032132a87786722459a1
alexperez45 <alperez296@gmail.com>
2024-06-04 20:41:18 -0400

    Added comments

--- Highest_Number.py
+++ Highest_Number.py
@@ -2,7 +2,7 @@ print ("Type some numbers to get the highest one: ")
 numbers = input().split()
 for n in range(0, len(numbers)):
   numbers[n] = int(numbers[n])
-
+# Calculates the highest number
 highest_number = 0
 for score in numbers:
   if score > highest_number:
--- Leap_year.py
+++ Leap_year.py
@@ -1,6 +1,6 @@
 # User enters year
 year = int(input("Type a year: "))
-
+# Make calculation to determine if year is a leap year
 if year % 4 > 0:
   print (f"{year} is NOT a leap year")
 elif year % 100 > 0:
